68a35782a12340ecaac32b24cc2f31e1cd512747
James Badu <baduj11@gmail.com>
2026-05-14 23:24:26 -0400

    Add files via upload

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <James Badu>
+ *        Date: <5/13/2026>
+ * Description: question 15 solution
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
